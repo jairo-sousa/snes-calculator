adcc38a424f5e9648d73e85f06d2f5a1b575e2c5
jairo sousa <jairo.sousa.rufino@gmail.com>
2022-12-29 22:21:15 -0300

    feat: keep the input always in focus

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ window.onload = function () {
 	const displayedNumber = document.querySelector("#displayedNumber");
 
 	displayedNumber.focus();
+	displayedNumber.onblur = function () {
+		displayedNumber.focus();
+	};
 };
