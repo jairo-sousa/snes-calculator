f0ce5c1fcae9157c4ab1e82dcec057eff2b05ab3
jairo sousa <jairo.sousa.rufino@gmail.com>
2022-12-31 12:10:00 -0300

    refactor: remove window.onload

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
-window.onload = function () {
-	const displayedContent = document.querySelector("#displayedContent");
+const displayedContent = document.querySelector("#displayedContent");
 
+displayedContent.focus();
+displayedContent.onblur = function () {
 	displayedContent.focus();
-	displayedContent.onblur = function () {
-		displayedContent.focus();
-	};
-
-	displayedContent.oninput = function () {
-		//TODO Filter numbers
-	};
-
-	function evaluate(expression) {
-		//WARNING this function presents security risks
-		try {
-			if (expression.match(/[a-zA-z&#$<>{}]/g)) throw new Error();
-			return new Function(`return (${expression})`)();
-		} catch (e) {
-			return null;
-		}
-	}
-	window.addEventListener("keydown", (keyEvent) => {
-		try {
-			comand = {
-				Enter: () => {
-					displayedContent.value = evaluate(displayedContent.value);
-				},
-				"=": () => {
-					displayedContent.value = evaluate(displayedContent.value);
-				},
-			}[`${keyEvent.key}`]();
-		} catch (e) {
-			return null;
-		}
-	});
 };
+
+displayedContent.oninput = function () {
+	//TODO Filter numbers
+};
+
+function evaluate(expression) {
+	//WARNING this function presents security risks
+	try {
+		if (expression.match(/[a-zA-z&#$<>{}]/g)) throw new Error();
+		return new Function(`return (${expression})`)();
+	} catch (e) {
+		return null;
+	}
+}
+window.addEventListener("keydown", (keyEvent) => {
+	try {
+		comand = {
+			Enter: () => {
+				displayedContent.value = evaluate(displayedContent.value);
+			},
+			"=": () => {
+				displayedContent.value = evaluate(displayedContent.value);
+			},
+		}[`${keyEvent.key}`]();
+	} catch (e) {
+		return null;
+	}
+});
