e33f7c6475a53d5311a77ac76616e9ac04e0dd9e
jairo sousa <jairo.sousa.rufino@gmail.com>
2022-12-31 12:12:22 -0300

    refactor: reorder functions

--- main.js
+++ main.js
@@ -9,15 +9,6 @@ displayedContent.oninput = function () {
 	//TODO Filter numbers
 };
 
-function evaluate(expression) {
-	//WARNING this function presents security risks
-	try {
-		if (expression.match(/[a-zA-z&#$<>{}]/g)) throw new Error();
-		return new Function(`return (${expression})`)();
-	} catch (e) {
-		return null;
-	}
-}
 window.addEventListener("keydown", (keyEvent) => {
 	try {
 		comand = {
@@ -32,3 +23,13 @@ window.addEventListener("keydown", (keyEvent) => {
 		return null;
 	}
 });
+
+function evaluate(expression) {
+	//WARNING this function presents security risks
+	try {
+		if (expression.match(/[a-zA-z&#$<>{}]/g)) throw new Error();
+		return new Function(`return (${expression})`)();
+	} catch (e) {
+		return null;
+	}
+}
