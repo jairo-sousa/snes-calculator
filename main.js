2695bd7284132fb92edf4cfb3c9735627be5aafc
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-01-04 17:04:26 -0300

    hotfix: hide select to copy process

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ const displayedContentStatus = {
 	clearWhenInput: false,
 };
 
+copyBtn.onmouseover = function () {
+	hideSelection(displayedContent);
+};
+
+copyBtn.onmouseout = function () {
+	hideSelection(displayedContent);
+};
+
 copyBtn.onclick = function () {
 	if (displayedContent.value) {
 		copyFromImput(displayedContent);
@@ -150,12 +158,10 @@ function clearDisplay(display) {
 }
 
 function copyFromImput(input) {
-	hideSelection(input);
 	input.select();
 	input.setSelectionRange(0, 99999);
 	document.execCommand("copy");
 	window.getSelection().removeAllRanges();
-	hideSelection(input);
 }
 
 function hideSelection(element) {
